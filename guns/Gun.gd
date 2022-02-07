3431a308440ad98c2b5005d866547865bac0039a
Guilherme Samuel <guisamuel800@gmail.com>
2022-02-07 10:47:47 -0300

    feat: gun shooting

--- guns/Gun.gd
+++ guns/Gun.gd
@@ -1,15 +1,32 @@
 extends Sprite
 
+# Constants
+const MUZZLE_POS_FLIP_TRUE = 4.625
+const MUZZLE_POS_FLIP_FALSE = -4.8
+
 # Variables
-var can_fire = false
+var can_fire = true
+
+# Scene Referecing
+var bullet = preload("res://guns/Bullet.tscn")
+
+# Nodes Referecing
+onready var muzzle = $Muzzle
 
 func _ready():
 	set_as_toplevel(true)
 
-func shotting() -> void:
-	
 
-func _physics_process(delta):
+func shotting() -> void: # Shotting with gun
+	var bullet_instance = bullet.instance()
+	bullet_instance.rotation = rotation
+	bullet_instance.global_position = muzzle.global_position
+	get_parent().add_child(bullet_instance)
+	can_fire = false
+	yield(get_tree().create_timer(0.2), "timeout")
+	can_fire = true
+
+func _physics_process(_delta):
 	global_position.x = lerp(global_position.x, get_parent().global_position.x, 0.4)
 	global_position.y = lerp(global_position.y, get_parent().global_position.y - 90, 0.4)
 	
@@ -19,8 +36,11 @@ func _physics_process(delta):
 	# Flipping gun
 	if get_parent().get_local_mouse_position().x < 0:
 		flip_v = true
+		muzzle.position.y = MUZZLE_POS_FLIP_TRUE
 	else:
 		flip_v = false
-		
-	if Input.is_action_pressed("ui_fire"):
+		muzzle.position.y = MUZZLE_POS_FLIP_FALSE
+	
+	# Press the left button mouse to shoot
+	if Input.is_action_pressed("ui_fire") && can_fire:
 		shotting()
