607f8d026c5cecc726428e3791e2263c973e7594
Guilherme Samuel <guisamuel800@gmail.com>
2022-02-07 11:45:40 -0300

    refactor: gun's recoil

--- guns/Gun.gd
+++ guns/Gun.gd
@@ -6,6 +6,7 @@ const MUZZLE_POS_FLIP_FALSE = -4.8
 
 # Variables
 var can_fire = true
+var recoil = 30
 
 # Scene Referecing
 var bullet = preload("res://guns/Bullet.tscn")
@@ -18,6 +19,7 @@ func _ready():
 
 
 func shotting() -> void: # Shotting with gun
+	global_position.x = lerp(global_position.x - recoil, get_parent().global_position.x, 0.7)
 	var bullet_instance = bullet.instance()
 	bullet_instance.rotation = rotation
 	bullet_instance.global_position = muzzle.global_position
@@ -37,9 +39,11 @@ func _physics_process(_delta):
 	if get_parent().get_local_mouse_position().x < 0:
 		flip_v = true
 		muzzle.position.y = MUZZLE_POS_FLIP_TRUE
+		recoil = 30
 	else:
 		flip_v = false
 		muzzle.position.y = MUZZLE_POS_FLIP_FALSE
+		recoil = -30
 	
 	# Press the left button mouse to shoot
 	if Input.is_action_pressed("ui_fire") && can_fire:
