ad413ae4c0283dc735729d80b6089d0c5d2c5578
Guilherme Samuel <guisamuel800@gmail.com>
2022-02-28 01:49:24 -0300

    style: sprite bullet enemy and ajustments

--- guns/EnemyGun.gd
+++ guns/EnemyGun.gd
@@ -8,13 +8,13 @@ const RECOIL = 20
 # Variables
 var can_fire = true
 var velocity = Vector2(0, 0)
-var mouse_pos = Vector2(0, 0)
 
 # Scene Referecing
-var bullet = preload("res://others/bullet/Bullet.tscn")
+var bullet = preload("res://others/bullet/EnemyBullet.tscn")
 
 # Nodes Referecing
 onready var muzzle = $Muzzle
+onready var player_pos = get_tree().current_scene.get_node("Player")
 
 func _ready() -> void:
 	set_as_toplevel(true)
@@ -30,28 +30,28 @@ func shotting() -> void: # Shotting with gun
 	velocity = velocity + kickdirection
 	
 	get_parent().add_child(bullet_instance)
-	get_parent().screen_shake.shake(0.2, 2)
 	can_fire = false
-	yield(get_tree().create_timer(0.2), "timeout")
+	yield(get_tree().create_timer(0.5), "timeout")
 	can_fire = true
 
 func _physics_process(_delta: float) -> void:
 	position += velocity
 	velocity = velocity * 0.7
-	global_position.x = lerp(global_position.x, get_parent().global_position.x, 0.4)
-	global_position.y = lerp(global_position.y, get_parent().global_position.y - 90, 0.4)
 	
-	mouse_pos = get_global_mouse_position()
-	look_at(mouse_pos)
+	global_position.x = lerp(global_position.x, get_parent().global_position.x, 0.4)
+	global_position.y = lerp(global_position.y, get_parent().global_position.y + 40, 0.4)
+	
+	look_at(player_pos.global_position)
 	
 	# Flipping gun
-	if get_parent().get_local_mouse_position().x < 0:
+	if player_pos.global_position.x < global_position.x:
 		flip_v = true
 		muzzle.position.y = MUZZLE_POS_FLIP_TRUE
+		rotation_degrees += 20
 	else:
 		flip_v = false
 		muzzle.position.y = MUZZLE_POS_FLIP_FALSE
+		rotation_degrees -= 20
 	
-	# Press the left button mouse to shoot
-	if Input.is_action_pressed("ui_fire") && can_fire:
+	if get_parent().founded && can_fire:
 		shotting()
