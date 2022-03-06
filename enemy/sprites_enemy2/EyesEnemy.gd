ac07d05863795ca15d71f4c60ff075e888120ae1
Guilherme Samuel <guisamuel800@gmail.com>
2022-03-06 18:52:58 -0300

    style: trying to enemy's eye follow player

--- enemy/sprites_enemy2/EyesEnemy.gd
+++ enemy/sprites_enemy2/EyesEnemy.gd
@@ -1,19 +1,19 @@
 extends Sprite
 
 # Nodes Referencing
-onready var child = get_children()[0]
+onready var eyes = get_children()[0]
+onready var player = get_tree().get_current_scene().get_node("Player")
 
 # Constants
 const MAX_EYE_UP = -3
 
 # Variables
-var max_dist = 15
+var max_dist = 10
 
 func _process(_delta) -> void:
 	# Eyes following mouse
-	var mouse_pos = get_parent().position
-	var dir = Vector2.ZERO.direction_to(mouse_pos)
-	var dist = mouse_pos.length()
+	var dist = global_position.distance_to(player.global_position)
+	var dir = Vector2.direction_to(player.position)
 	#if mouse_pos.y < MAX_EYE_UP:
 	#	dir.y = 0
-	child.position = dir * min(dist, max_dist)
+	eyes.position = dir * min(dist, max_dist)
