491db3b794def9bc5ffd615a31839a2f99e119a6
Guilherme Samuel <guisamuel800@gmail.com>
2022-03-19 17:07:24 -0300

    fix: aim

--- others/mouse/Mouse.gd
+++ others/mouse/Mouse.gd
@@ -3,5 +3,5 @@ extends AnimatedSprite
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
-func _process(delta):
+func _process(_delta):
 	global_position = get_global_mouse_position()
