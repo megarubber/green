f008cb07a73bb210d148428be129b5598e62bbb4
Guilherme Samuel <guisamuel800@gmail.com>
2022-04-01 14:26:58 -0300

    feat: checkpoint animation

--- others/checkpoint/Checkpoint.gd
+++ others/checkpoint/Checkpoint.gd
@@ -1,5 +1,16 @@
 extends Area2D
 
-func _on_Checkpoint_body_entered(body):
-	if body.is_in_group("player"):
+# Node Referencing
+onready var anim = $AnimationPlayer
+onready var label = $Label
+
+func _ready() -> void:
+	label.visible = false
+	if Global.is_checkpoint_hitted:
+		anim.play("already_checked")
+
+func _on_Checkpoint_body_entered(body) -> void:
+	if body.is_in_group("player") && !Global.is_checkpoint_hitted:
 		body.hit_checkpoint()
+		label.visible = true
+		anim.play("checked")
