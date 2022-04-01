fcd3c101c57b49e5fc18d262af83446b5d1b623e
Guilherme Samuel <guisamuel800@gmail.com>
2022-03-31 23:01:40 -0300

    feat: start levels name display

--- others/hud/HUD.gd
+++ others/hud/HUD.gd
@@ -1,18 +1,30 @@
 extends CanvasLayer
 
 # Node Referencing
-onready var black_screen_anim = $GameOver/AnimationPlayer
+onready var game_over_anim = $GameOver/AnimationPlayer
 onready var game_over_screen = $GameOver
+onready var start_anim = $StartLevel/AnimationPlayerStart
+onready var start = $StartLevel
+onready var level_name_label = $StartLevel/LevelName
+onready var level_number_label = $StartLevel/LevelNumber
 
-func _ready():
+func _ready() -> void:
+	var level_name_reference = get_tree().get_current_scene().get_name()
+	match level_name_reference:
+		"Level1":
+			level_number_label.set_text("Stage 01")
+			level_name_label.set_text("Samueland City")
 	var player = get_tree().get_current_scene().get_node("Player")
 	player.connect("player_death", self, "_player_death")
 	game_over_screen.visible = false
-
-func _player_death():
+	start_anim.play("start")
+	
+func _player_death() -> void:
 	game_over_screen.visible = true
-	black_screen_anim.play("fade_in")
+	game_over_anim.play("fade_in")
 
-func _on_AnimationPlayer_animation_finished(_anim_name):
-	#black_screen_anim.play("black")
-	black_screen_anim.stop()
+func _on_AnimationPlayer_animation_finished(_anim_name) -> void:
+	game_over_anim.stop()
+
+func _on_AnimationPlayerStart_animation_finished(_anim_name) -> void:
+	start.queue_free()
