ba6f1633c52174b1badf045129a45944e16a655a
Guilherme Samuel <guisamuel800@gmail.com>
2022-04-14 17:28:28 -0300

    feat: settings screen (elements & buttons)

---
+++ main_menu/Settings.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+# Node Referencing
+onready var start_game = $BtnStartGame
+onready var settings = $BtnSettings
+onready var credits = $BtnCredits
+onready var exit = $BtnExit
+onready var transition = $TransitionBlock
+
+func _ready() -> void:
+	enable_buttons(false)
+	transition.fade_out()
+	yield(transition, "finished")
+	enable_buttons(true)
+	start_game.grab_focus()
+
+func enable_buttons(value : bool) -> void:
+	start_game.disabled = !value
+	settings.disabled = !value
+	credits.disabled = !value
+	exit.disabled = !value
+
+func _on_BtnExit_pressed() -> void:
+	get_tree().quit()
+
+func _on_BtnStartGame_pressed() -> void:
+	transition.fade_in()
+	yield(transition, "finished")
+	var _scene = get_tree().change_scene("res://levels/Level1.tscn")
